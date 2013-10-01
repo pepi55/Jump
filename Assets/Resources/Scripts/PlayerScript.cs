f685889dda4e03554a988f5a67964e982d9f3920
DutchBeastman <Fabianverk@hotmail.com>
2013-10-01 15:11:49 +0200

    Player update

--- Assets/Resources/Scripts/PlayerScript.cs
+++ Assets/Resources/Scripts/PlayerScript.cs
@@ -10,18 +10,13 @@ public class PlayerScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		
-		
-		
-		float horizontalMovement = Input.GetAxis("Horizontal");
-		float verticalMovement = Input.GetAxis("Jump");
-		Vector3 movement = new Vector3(horizontalMovement, verticalMovement, 0.0f);
-		rigidbody.AddForce(movement * speed * Time.deltaTime);	
+		if (Input.GetAxis("Horizontal") == 1)
+		{
+			float horizontalMovement = Input.GetAxis("Horizontal");
+			float verticalMovement = Input.GetAxis("Jump");
+			Vector3 movement = new Vector3(horizontalMovement, verticalMovement, 0.0f);
 			
-			
-			
-			
-			
-		
+			rigidbody.AddForce(movement * speed * Time.deltaTime);
+		}
 	}
 }
