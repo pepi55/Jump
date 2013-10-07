8070668d962595b008c0f6b6387e08d17d0981bb
Pepi Dimi <pepi55@live.com>
2013-10-07 12:18:14 +0200

    Sprite script added

---
+++ Assets/Resources/Scripts/SpriteAnimate.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteAnimate : MonoBehaviour {
+	
+	public int cols;
+	public int rows;
+	public int cells;
+	public int fps;
+	
+	string mainTex = "_MainTex";
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int index = (int) (fps * Time.time);
+		index = index % cells;
+		
+		float uIndex = index % cols;
+		float vIndex = index / cols;
+		
+		float sizeX = 1f / cols;
+		float sizeY = 1f / rows;
+		Vector2 size = new Vector2(sizeX, sizeY);
+		
+		float offsetX = 1f / cols * uIndex;
+		float offsetY = 1f / rows * vIndex;
+		Vector2 offset = new Vector2(offsetX, offsetY);
+		
+		renderer.material.SetTextureScale(mainTex, size);
+		renderer.material.SetTextureOffset(mainTex, offset);
+	}
+}
